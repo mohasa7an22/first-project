b99d028597aab667db94687b0944dbbb940a9d6d
mohasa7an22 <mohasar7an22@gmail.com>
2026-06-29 11:27:38 +0300

    added shuffle function which shuffles all 52 cards without duplicates

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,30 @@
-const suits = ['hearts', 'spades', 'clubs', 'diamonds']
-const cards = [2 , 3 , 4 , 5 ,6 ,7 ,8 ,9 ,10 ,'J' , 'Q' , 'K', 'A']
-let deck = [], playerHand = [], dealerHand = []
-let balance = 500
+// variables
 
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let deck = [],
+  playerHand = [],
+  dealerHand = [];
+let balance = 500;
+let bet = 0;
+
+// functions
+
+function shuffle() {
+  for (let i = 0; i < deck.length;  i++) {
+    const newShuffle = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[newShuffle]] = [deck[newShuffle], deck[i]]
+  }
+}
 function createDeck() {
   deck = [];
   for (let symbol of suits)
-    for (let number of cards)
-      deck.push({ symbol, number });
-  }
-createDeck()
-console.log(deck)
-console.log(deck.length)
+    for (let number of cards) 
+        deck.push({ symbol, number });
+    shuffle();
+}
+
+
+createDeck();
+console.log(deck);
+console.log(deck.length);
