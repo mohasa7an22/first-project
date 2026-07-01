4390adaaa568c018d52e80f44dc4d314f42c79b9
mohasa7an22 <mohasar7an22@gmail.com>
2026-07-01 10:45:08 +0300

    Finished the Functions entirely including dealing cards, checking for winners, making the game end when it is supposed to end, added render function to plenty of function so that when the game starts the functions are working properly, changed the '$' to 'P' so that you deal with points instead of money,made cards using CSS combined with javascript html codes, seperated red cards and black cards with having variables for color that change with the function of dealCards

--- javascript/app.js
+++ javascript/app.js
@@ -22,6 +22,8 @@ console.log(betBtn)
 
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const icons = {'hearts': '♥️', 'spades': '♠️', 'clubs': '♣️', 'diamonds': '♦️'}
+const redCard = ['hearts', 'diamonds']
 let deck = [];
 let playerHand = [];
 let dealerHand = [];
@@ -33,7 +35,7 @@ let wins = 0;
 // functions
 
 function shuffle() {
-  for (let i = 0; i < deck.length; i++) {
+  for (let i = deck.length -1; i > 0; i--) {
     const newShuffle = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[newShuffle]] = [deck[newShuffle], deck[i]];
   }
@@ -99,12 +101,12 @@ function startGame() {
     hand.push(draw()),
   );
   stage = "playing";
-
+  render(true)
   const playerBj = blackJackHand(playerHand)
   const dealerBj = blackJackHand(dealerHand)
 
   if (playerBj && dealerBj){
-    setStatus('you both have a BlackJack it is a tie')
+    setStatus('You both have a BlackJack it is a tie')
   }
 
   else if (playerBj){
@@ -114,13 +116,17 @@ function startGame() {
     setStatus('Dealer has BlackJack the house always wins')
   }
   else{
-    setStatus('hit or stay') 
+    setStatus('Hit or Stay') 
   }
 }
 
 function bustedHand(){
   if(finalScore(playerHand) > 21){
     setStatus('You busted')
+    endRound(-1)
+  }
+  else{
+    render(true)
   }
 }
 
@@ -133,31 +139,142 @@ function hitMe(){
 }
 
 function stay(){
-  if (stage !== 'playing'){
-    return
+  if (stage !== 'playing') return
     dealerPlay()
-  }
-}
 
+}
+function clearBet(){
+  if (stage !== 'betting') return
+  balance += bet
+  bet = 0
+  setStatus('Cleared bet')
+  render()
+}
 function dealerPlay(){
   if(finalScore(dealerHand) < 17){
     dealerHand.push(draw())
-    return dealerPlay
+    return dealerPlay()
   }
+  
+  const playerScore = finalScore(playerHand)
+  const dealerScore = finalScore(dealerHand)
+  
+  if(dealerScore > 21 || playerScore > dealerScore){
+    setStatus(`You won ${bet}P!`)
+    endRound(1)
+  }
+  
+  else if (playerScore < dealerScore){
+    setStatus('The house always wins ')
+    endRound(-1)
+  }
+  else {
+    setStatus('It is a tie')
+    endRound(0)
+  }
+   render()
 }
 
+function endRound(results){
+  stage = 'betting'
+  if (results === 1) {
+    balance += bet * 2; wins++
+  }
+  else if (results === 0){
+    balance += bet
+  }
+  bet = 0 
+  if (balance <= 0){
+    let time = 5
+    setStatus(`out of chips restarting in ${time}`)
+    const timer = setInterval (()=> {
+      time --
+      setStatus(`out of chips restarting in ${time}`)
+      if (time <= 0){
+        clearInterval(timer)
+        location.reload()
+      }
+    }, 1000)  
+  
+    }
+    render()
+  }
+
+function dealCards(hand, hide){
+  let msg = ''
+  for(let i = 0; i < hand.length; i ++){
+    const crd = hand[i]
+    if  (hide && i === 1){
+      msg += `<li><article class ="card-back" aria-label="hidden card"></article></li>`
+    } 
+else {
+  let cardColor
+  if (redCard.includes(crd.symbol)) {
+    cardColor = 'red'
+  } else {
+    cardColor = 'black'
+  }
+  msg += `<li>
+    <article class="card ${cardColor}" aria-label="${crd.number} of ${crd.symbol}">
+    <span class="corner"> ${crd.number} ${icons[crd.symbol]} </span>
+    </article>
+  </li>`
+}
+  }
+  return msg
+}
+function placeBet(points){
+  if (stage !== 'betting') return
+
+  if (points > balance){
+    setStatus('Not enough points')
+    return
+  }
+    bet += points 
+    balance -= points
+    setStatus(`Bet: ${bet}P - click start`)
+  render()
+}
+function render (dealerHidden = false) {
+  let dealerScoreText
+  if (dealerHidden){
+    dealerScoreText = '?'
+  }
+  else{
+    dealerScoreText = finalScore(dealerHand)
+  }
+  document.getElementById('player').innerHTML = `
+  <section class = "hand">
+  <h3> Your Hand <span> (${finalScore(playerHand)})</span></h3>
+  <ul role = "list"> ${dealCards(playerHand, false)}</ul>
+  </section>
+  <section class ="hand">
+  <h3> Dealers hand <span> (${dealerScoreText}) </span> </h3>
+  <ul role = "list"> ${dealCards(dealerHand, dealerHidden)}</ul>
+  </section>
+  <footer class= "stats">
+  <p> Balance: <strong> ${balance}P</strong></p>
+  <p> Bet: <strong> ${bet}P</strong></p>
+  <p> Wins: <strong> ${wins}P</strong></p>
+  </footer>
+  `
+  const phase = stage === 'playing'
+  btnStart.disabled = phase
+  btnHit.disabled = !phase
+  btnStay.disabled = !phase
+}
 // event listeners
 
 btnStart.addEventListener('click' ,startGame)
 btnHit.addEventListener('click', hitMe)
+btnStay.addEventListener('click', stay)
+btnClearBet.addEventListener('click', clearBet)
+five.addEventListener('click', () => placeBet(5))
+ten.addEventListener('click', () => placeBet(10))
+twoFive.addEventListener('click', () => placeBet(25))
+fifty.addEventListener('click', () => placeBet(50))
 
 //init and console checking
 createDeck();
-bet = 300;
-startGame();
-console.log("playerhand", playerHand);
-console.log("dealer:", dealerHand);
-console.log(deck);
-console.log(deck.length);
-console.log(stage);
-
+render()
+setStatus('Place a bet and start')
